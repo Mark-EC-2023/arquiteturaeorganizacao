05f17c35791ec4469c10b6fd4797588e40c17928
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-04-15 23:28:08 -0300

    Update Questao-7.asm

--- Capitulo_3/Questao-7.asm
+++ Capitulo_3/Questao-7.asm
@@ -9,21 +9,21 @@ msg1fmt byte 	0Ah,"%s",0
 msg2fmt byte    0Ah,"%s%d",0Ah,0
 msg1	byte	"Enter the degrees in Fahrenheit: ",0
 msg2	byte	"The degrees in Celsius is: ",0
-F	sdword  ?
-C	sdword	?
+f	sdword  ?
+c	sdword	?
 	.code
 main	proc
-	INVOKE printf, ADDR msg1fmt, ADDr msg1
-	INVOKE scanf, ADDR in1fmt, ADDR F
-	mov eax,F
+	INVOKE printf, ADDR msg1fmt, ADDR msg1
+	INVOKE scanf, ADDR in1fmt, ADDR f
+	mov eax,f
 	sub eax,32
 	mov ebx,5
 	imul ebx
 	mov ebx,9
 	cdq
 	idiv ebx
-	mov C,eax
-	INVOKE printf, ADDR msg2fmt, ADDR msg2, C
+	mov c,eax
+	INVOKE printf, ADDR msg2fmt, ADDR msg2, c
 	ret
 main	endp
 	end	 
