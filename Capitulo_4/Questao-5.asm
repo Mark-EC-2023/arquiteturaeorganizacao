c4ddd0a19073f8ef89fd0d5044f9e79566f42940
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-05-01 16:50:18 -0300

    Update Questao-5.asm

--- Capitulo_4/Questao-5.asm
+++ Capitulo_4/Questao-5.asm
@@ -5,6 +5,8 @@
 .686
           .model    flat,c
           .stack    100h
+printf    PROTO	arg1:Ptr Byte, printlist:VARARG
+scanf     PROTO	arg2:Ptr Byte, inputlist:VARARG
           .data
 count     sdword    ?
 number    sdword    ?
