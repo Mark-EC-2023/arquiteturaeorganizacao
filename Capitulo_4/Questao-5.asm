27e66c0b5d7185539d8102f878d66ff33886da7f
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-05-01 15:24:30 -0300

    Create Questao-5.asm

---
+++ Capitulo_4/Questao-5.asm
@@ -0,0 +1,46 @@
+.686
+          .model    flat,c
+          .stack    100h
+          .data
+count     sdword    ?
+number    sdword    ?
+
+infmt:    byte      "%d",0
+msg1fmt:  byte      0Ah,"%s",0Ah,0
+msg1:     byte      "Digite o valor de number: ",0
+msg2fmt:  byte      0Ah,"%s%d",0
+msg2:     byte      "O valor final de count eh: "
+
+          .code
+main      proc
+          INVOKE    printf, ADDR msg1fmt,ADDR msg1
+          INVOKE    scanf, ADDR infmt, ADDR number
+
+          mov count,10
+          
+ifBase:   cmp number,0
+          jl teste
+          cmp number,3
+          jg teste
+          
+if00:     cmp number,0
+then00:   je  if01
+endif00:  nop
+
+if01:     cmp count,1
+then01:   add count,2
+endif01:  jmp teste
+
+if02:     cmp count,2
+then02:   je  if03
+endif02:  nop
+
+if03:     cmp count,3
+then03:   sub count,2
+endif03:  jmp teste
+
+teste:    nop
+          INVOKE printf, ADDR msg2fmt, ADDR msg2, ADDR count
+          ret
+main      endp
+end
