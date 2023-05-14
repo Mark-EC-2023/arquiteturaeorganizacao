822bf16053584f31d3553f170c6be6c41c0c6bd4
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-05-14 15:26:24 -0300

    pronto p teste

--- Capitulo_4/Questão-2.asm
+++ Capitulo_4/Questão-2.asm
@@ -1,13 +1,37 @@
         .686
         .model   flat,c
         .stack   100h
+printf  PROTO	arg1:Ptr Byte, printlist:VARARG
+scanf	PROTO	arg2:Ptr Byte, inputlist:VARARG
         .data
+infmt   byte    "%d",0
+msgfmt  byte    "%s",0Ah,
+msga    byte    "Digite o valor de a eh ",0
+msgb    byte    "Digite o valor de b eh ",0
+msgc    byte    "Digite o valor de c eh ",0
+msgd    byte    "Digite o valor de d eh ",0
+msg2ft  byte    0Ah,"%s%d,%d,%d,%d",0
+msg2    byte    "Os valores finais de a,b,c,d sao respectivamente: "
 a       sdword   ?
 b       sdword   ?
 c       sdword   ?
 d       sdword   ?
+
         .code
 main    proc      
+        
+        INVOKE printf, ADDR msgfmt, ADDR msga
+        INVOKE scanf, ADDR infmt, ADDR a
+       
+        INVOKE printf, ADDR msgfmt, ADDR msgb
+        INVOKE scanf, ADDR infmt, ADDR b
+        
+        INVOKE printf, ADDR msgfmt, ADDR msgc
+        INVOKE scanf, ADDR infmt, ADDR c
+
+        INVOKE printf, ADDR msgfmt, ADDR msgd
+        INVOKE scanf, ADDR infmt, ADDR d
+
         .if a > b ;if(a)
           dec a
         .else
@@ -27,6 +51,10 @@ main    proc
                   .endif   ;endif(c)
                .endif ;endif(b)
         .endif  ;endif(a)
+        
+        INVOKE printf, ADDR msg2ft, ADDR msg2, a,b,c,d
+        
+        
         ret
 main    endp
 end
