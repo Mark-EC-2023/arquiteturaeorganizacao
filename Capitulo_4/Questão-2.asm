ef8055797d58896d46aaecbf44b3eb5799d253a4
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-05-14 15:35:09 -0300

    Update Questão-2.asm

--- Capitulo_4/Questão-2.asm
+++ Capitulo_4/Questão-2.asm
@@ -16,52 +16,52 @@ msgc    byte    "Digite o valor de c eh ",0
 msgd    byte    "Digite o valor de d eh ",0
 msg2ft  byte    0Ah,"%s%d,%d,%d,%d",0
 msg2    byte    "Os valores finais de a,b,c,d sao respectivamente: "
-a       sdword   ?
-b       sdword   ?
-c       sdword   ?
-d       sdword   ?
+ax       sdword   ?
+bx      sdword   ?
+cx      sdword   ?
+dx      sdword   ?
 
         .code
 main    proc      
         
         INVOKE printf, ADDR msgfmt, ADDR msga
-        INVOKE scanf, ADDR infmt, ADDR a
+        INVOKE scanf, ADDR infmt, ADDR ax
        
         INVOKE printf, ADDR msgfmt, ADDR msgb
-        INVOKE scanf, ADDR infmt, ADDR b
+        INVOKE scanf, ADDR infmt, ADDR bx
         
         INVOKE printf, ADDR msgfmt, ADDR msgc
-        INVOKE scanf, ADDR infmt, ADDR c
+        INVOKE scanf, ADDR infmt, ADDR cx
 
         INVOKE printf, ADDR msgfmt, ADDR msgd
-        INVOKE scanf, ADDR infmt, ADDR d
+        INVOKE scanf, ADDR infmt, ADDR dx
 
 
         ; INICIO DA QUESTAO
         
-        .if a > b ;if(a)
-          dec a
+        .if ax > bx ;if(a)
+          dec ax
         .else
-          .if b >= c ;if(b)
-            sub b,2
+          .if bx >= cx ;if(b)
+            sub bx,2
             .else
-              .if c > d ; if(c)
-                mov eax,c
-                add eax,d
-                mov c,eax
+              .if cx > dx ; if(c)
+                mov eax,cx
+                add eax,dx
+                mov cx,eax
                 .else
                   mov ebx,2
-                  mov eax,d
+                  mov eax,dx
                   cdq 
                   idiv ebx
-                  mov d,eax
+                  mov dx,eax
                   .endif   ;endif(c)
                .endif ;endif(b)
         .endif  ;endif(a)
         
         ; FIM DA QUESTAO
         
-        INVOKE printf, ADDR msg2ft, ADDR msg2, a,b,c,d
+        INVOKE printf, ADDR msg2ft, ADDR msg2, ax,bx,cx,dx
         
         
         ret
