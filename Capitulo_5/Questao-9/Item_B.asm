c81b91585f8f96f5375a337a5179a12887ee244a
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-06-03 23:54:55 -0300

    Update Item_B.asm

--- Capitulo_5/Questao-9/Item_B.asm
+++ Capitulo_5/Questao-9/Item_B.asm
@@ -2,7 +2,7 @@
 	      .model 	flat,c
 	      .stack 	100h
 printf	PROTO 	arg1:Ptr Byte, printlist:VARARG
-scanf	  PROTO	arg2:Ptr Byte, inputlist:VARARG
+scanf	PROTO	arg2:Ptr Byte, inputlist:VARARG
 	      .data
 infmt   byte "%d",0 
 msg1fmt byte 0Ah,"%s",0Ah,0
@@ -13,13 +13,13 @@ n       sdword  ?
 var     sdword ?
         .code
         main	proc
-	      INVOKE printf, ADDR msg1fmt, ADDR msg1
-	      INVOKE scanf, ADDR infmt, ADDR n
+	INVOKE printf, ADDR msg1fmt, ADDR msg1
+	INVOKE scanf, ADDR infmt, ADDR n
         mov edx,n
         ; testar se é 0 ou 1
         cmp edx,1
 if0or1: jle cs01
-        mov ecx,n
+        mov ecx,1
         
 	mov var,1
         .repeat
@@ -27,9 +27,9 @@ if0or1: jle cs01
 	mov eax,var
 	imul ecx
 	mov var,eax
-	dec ecx
+	inc ecx
         
-        .until  ecx <= 0
+        .until  ecx > 0
         
 	mov eax,ebx
         jmp fim
