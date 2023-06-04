1e2aebbe7cd284d8525962ee4b4b4968c88c0116
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-06-03 21:07:16 -0300

    Update Questao-2.asm

--- Capitulo_5/Questao-2.asm
+++ Capitulo_5/Questao-2.asm
@@ -1,3 +1,23 @@
+          .686
+          .model    flat,c
+          .stack    100h
+printf    PROTO	arg1:Ptr Byte, printlist:VARARG
+scanf     PROTO	arg2:Ptr Byte, inputlist:VARARG
+          .data
+ans     sdword    ?
+x       sdword    ?
+y       sdword    ?  
+
+infmt:    byte      "%d%d%d",0
+msg1fmt:  byte      0Ah,"%s",0Ah,0
+msg1:     byte      "Digite o valor de x,y,ans: ",0
+msg2fmt:  byte      0Ah,"%s%d%d%d",0Ah,0
+msg2:     byte      "O valor final de x,y,ans eh : ",0
+
+          .code
+main      proc
+          INVOKE    printf, ADDR msg1fmt,ADDR msg1
+          INVOKE    scanf, ADDR infmt, ADDR x,ADDR y ,ADDR ans
           mov ans,0 ; initialize ans to 0
           mov eax,x
 if01:     cmp  x,0
@@ -13,3 +33,5 @@ then02:   jge endif02
 endif02:  nop
           mov i,ecx ; store ecx in i
 endif01:  nop
+
+INVOKE printf, ADDR msg2fmt,ADDR msg2,
