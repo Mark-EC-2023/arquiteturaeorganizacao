67ee499289a3a46e3426f1a48f2c5980e568878a
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-06-29 18:47:04 -0300

    Update Questao-4.asm

--- Capitulo_10/Questao-4.asm
+++ Capitulo_10/Questao-4.asm
@@ -12,13 +12,13 @@ out2fmt byte    Ah0,"%s6.4Lf",Ah0,Ah0,0
 out1    byte    "Enter a long double for x: ",0
 out2    byte    "The long double in y is: ",0
 	      .code
-main 	  proc
+main 	proc
         INVOKE printf, ADDR out1fmt, ADDR out1
         INVOKE scanf_s,ADDR infmt,ADDR x
         fld x
         fstp y
         INVOKE printf,ADDR out2fmt,ADDR out2, y
         
-	      ret
+	ret
 main    endp
 end
