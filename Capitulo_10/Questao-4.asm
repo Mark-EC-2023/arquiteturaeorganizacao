b48bb4beee796baa8a6e6a1476c72b8a1cd2ba7a
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-06-29 18:46:27 -0300

    Create Questao-4.asm

---
+++ Capitulo_10/Questao-4.asm
@@ -0,0 +1,24 @@
+        .686
+        .model 	flat, c
+        .stack 	100h
+printf 	PROTO arg1:Ptr Byte, printlist:VARARG
+scanf_s PROTO arg2:Ptr Byte, inputlist:VARARG
+.data
+x       real10  ?
+y       real10  ?
+infmt   byte    "%Lf",0
+out1fmt byte    Ah0,"%s",0
+out2fmt byte    Ah0,"%s6.4Lf",Ah0,Ah0,0
+out1    byte    "Enter a long double for x: ",0
+out2    byte    "The long double in y is: ",0
+	      .code
+main 	  proc
+        INVOKE printf, ADDR out1fmt, ADDR out1
+        INVOKE scanf_s,ADDR infmt,ADDR x
+        fld x
+        fstp y
+        INVOKE printf,ADDR out2fmt,ADDR out2, y
+        
+	      ret
+main    endp
+end
