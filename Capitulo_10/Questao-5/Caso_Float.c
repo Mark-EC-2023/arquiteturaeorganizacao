054bd524de222cdaaf2f488823cfbcc83e6ebefa
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-06-29 19:07:27 -0300

    Create Caso_Float.c

---
+++ Capitulo_10/Questao-5/Caso_Float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+float x,y;
+printf("\n%s", "Enter a float for x: ");
+scanf_s("%f", &x);
+__asm {
+fld x
+fstp y
+ }
+printf("\n%s%6.4f\n\n", "The float in y is: ", y);
+return 0;
+} 
