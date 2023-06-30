c492666eb65815070d50822752526b16be5dbfe8
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-06-29 23:10:08 -0300

    Create CodigoC.c

---
+++ Capitulo_10/Questão-6/CodigoC.c
@@ -0,0 +1,14 @@
+// Não sei se a questão quer o código em c ou assembly, então fiz os 2 
+#include <stdio.h>
+
+int main(){
+
+  int x,y;
+  scanf("%f%f",&x,&y);
+  if(x > y){
+    printf("\n%s\n", "x is greater than y");
+  }else{
+    printf("\n%s\n","x is less than or equal to y");
+  }
+  return 0; 
+}
