65c67fe760da846ecdec33ae15a33e4e90020a47
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2023-07-08 20:06:48 -0300

    Testado

--- Capitulo_10/Questao-9.asm
+++ Capitulo_10/Questao-9.asm
@@ -1,30 +1,52 @@
+;Testei
             .686
             .model flat, c
             .stack 100h
 printf      PROTO arg1:Ptr Byte, printlist:VARARG
 scanf       PROTO arg2:Ptr Byte, inputlist:VARARG
             .data
-in1fmt      byte "%lf%lf%lf%lf%lf",0
-inmsg       byte "Digite os valores",0Ah,0            
+in1fmt      byte "%lf",0
+inmsg       byte "Digite o valor da posicao %d : ",0
+pl          byte 0Ah,0
 msg1fmt     byte 0Ah,"%s",0Ah,0Ah,0
 msg2fmt     byte " %3.1f",0Ah,0
 msg1        byte "Array",0
 n           sdword 5
 array       real8 5 dup(?)
+aux         sdword 0
+leitor      real8 ?
             .code
 main        proc
-            lea esi,array 
-            INVOKE printf,ADDR inmsg
-            INVOKE scanf, ADDR in1fmt, ADDR esi+0,ADRR esi+8,ADDR esi+16,ADDR esi+24,ADDR esi+32
+            lea esi , array+0
+            mov ecx,5
+            
+            .while ecx > 0
+            
+            push ecx
+            inc aux
+            mov edi,esi
+
+            INVOKE printf,ADDR inmsg, aux 
+            INVOKE scanf, ADDR in1fmt, ADDR real8 ptr leitor
+            
+            pop ecx
+            fld leitor
+            fstp real8 ptr [edi]
+            add esi,8
+            dec ecx
+            .endw
+
             INVOKE printf, ADDR msg1fmt, ADDR msg1
             lea esi,array
             mov ecx,n
+                
             .repeat
             push ecx
             INVOKE printf, ADDR msg2fmt, real8 ptr [esi]
             pop ecx
             add esi,8
             .untilcxz
+
             INVOKE printf, ADDR msg1fmt, ADDR msg1
             mov ebx,0
             mov ecx,n
