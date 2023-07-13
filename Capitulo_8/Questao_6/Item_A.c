0997e846b3430eca558ff5e5fc15fffd45f4583c
Mark_10011 <129462229+Mark-EC-2023@users.noreply.github.com>
2023-07-12 21:32:52 -0300

    Update Item_A.c

--- Capitulo_8/Questao_6/Item_A.c
+++ Capitulo_8/Questao_6/Item_A.c
@@ -2,7 +2,6 @@
 
 int main(){
     int array1[20]={1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    
     for(int i=0; i<20; i++){
         for(int j=0; j<20; j++){
             if(array1[i] < array1[j]){
@@ -12,8 +11,6 @@ int main(){
             }
         }
     }
-    for(int i=0; i<20; i++)
-        printf("%d ", array1[i]);
 
-    return 0;
+    for(int i=0; i<20; i++)   printf("%d ", array1[i]);
 }
