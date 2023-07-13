16d8e175eba09d0c48ec0fc75298f87517936394
Mark_10011 <129462229+Mark-EC-2023@users.noreply.github.com>
2023-07-12 22:41:18 -0300

    Update Questao_3.asm

--- Capitulo_9/Questao_3.asm
+++ Capitulo_9/Questao_3.asm
@@ -1,1 +1,51 @@
-
+;Não está terminado ainda
+.686
+.model  flat, c
+.stack  100h
+printf PROTO arg1:Ptr Byte, printlist:VARARG
+scanf PROTO arg2:Ptr Byte, printlist:VARARG
+.data
+msglfmt  byte "%s", 0Ah, 0 
+inlfmt   byte "%s",  0
+msg1     byte "Enter a word: ", 0
+msg2     byte "The string is a palindrome ", 0
+msg3     byte "The string isn't a palindrome ", 0
+arraystr byte "1234554321 7234554328 abzdeedyba ummaggammu abcdeedcba fghijjhgf ababababab 5432154321 aaaaaaaaaa 1111111111"
+lenght   sdword 10
+found    sdword 0
+temp1    sdword 0
+temp2    sdword 0
+temp3    sdword 0
+.code 
+main        proc
+            lea esi, arraystr
+            lea edi, arraystr + 9
+            mov  eax, 0
+            .while (temp1<10)
+            mov found, 0
+                mov ecx, 4
+                .repeat
+                    mov al, [edi]
+                    mov ah, [esi]
+                    .if(al != ah)
+                        mov found, -1
+                    .endif
+                    inc esi
+                    dec edi
+                .untilcxz
+                add esi, 7
+                add edi, 15
+                mov temp2, esi
+                mov temp2, edi
+                .if(found <= -1)
+                        INVOKE printf, ADDR msg1fmt, ADDR msg3
+                      .else
+                        INVOKE printf, ADDR msg1fmt, ADDR msg2
+                .endif
+                mov esi, temp2
+                mov edi, temp3
+                inc temp1
+            .endw
+            ret
+main       endp
+           end
